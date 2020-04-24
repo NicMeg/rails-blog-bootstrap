e2506bc71f23308536d01c3558c48d86f4eb926f
Nic Meg <nicmeg54@gmail.com>
2020-04-24 16:15:04 +1000

    wip

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :summary, presence: true
+  validates :body, presence: true
+
+  belongs_to :category
+
+ after_create :update_total_posts_count
+
+ private
+
+  def update_total_posts_count
+  category.increment(:total_count, 1).save
+  end
+
 end
